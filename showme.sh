384be7af3754d63e89adf8afde4421ef8df9ee65
tr33t0psec <77170493+tr33t0psec@users.noreply.github.com>
2021-01-08 13:49:06 -0600

    added shell script
    added simple "clear terminal & show connected users" bash script for exercise and clarity.

---
+++ showme.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# runs 'w' for currently connected users, reflects current user
+# clears terminal windows and echoes activity
+clear				# clear the clutter
+
+echo "bashing..."
+
+echo "You are identified, $USER!"		# this says you are you
+echo
+
+echo "now displaying - connected user(s):"
+echo							
+w				# 
+echo				# what they are doing
+
+
+echo
+
+echo "Users listed, terminal cleared, well...until now :)"
+echo
